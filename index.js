d8e5142088221f1abdbfc862892ff4116d37f7d8
Grodriguezcervantes <grodriguezcervantes35@correo.unicordoba.edu.co>
2021-08-08 22:50:14 -0500

    ACTIVIDAD 3
    CLIENTE SERVIDO CON SOCKET

---
+++ index.js
@@ -0,0 +1,27 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+
+app.get('/', (req, res) => {  res.sendFile(__dirname + '/index.html');});
+
+http.listen(4000, () => {
+  console.log('listening on *:4000');
+});
+
+
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+      });
+});
+
+http.listen(4000, () => {
+  console.log('listening on *:4000');
+});
