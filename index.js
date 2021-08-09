12647490e39ce9b9c911a83b926fda02b041e1e4
Grodriguezcervantes <grodriguezcervantes35@correo.unicordoba.edu.co>
2021-08-08 23:31:12 -0500

    ACTIVIDAD 3

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-
-app.get('/', (req, res) => {  res.sendFile(__dirname + '/index.html');});
-
-http.listen(4000, () => {
-  console.log('listening on *:4000');
-});
-
-
-var app = require('express')();
-var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
@@ -18,9 +8,11 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-      });
-});
+        io.emit('chat message', msg);
+        
+      })
+    });
+
 
 http.listen(4000, () => {
   console.log('listening on *:4000');
