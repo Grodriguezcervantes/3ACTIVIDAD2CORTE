3438be0aaa5cba60cb244ce5911c4b3bcfe2c88e
RoberDurango <rdurangolozano60@correo.unicordoba.edu.co>
2021-08-10 23:30:39 +0000

    ACTIVIDAD 3

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+      
+    })
     });
 
     http.listen(4000, () => {
